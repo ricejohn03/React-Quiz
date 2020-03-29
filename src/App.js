9655558640c431e86996002a81079dc257fba794
ricejohn03 <john.rice.create@gmail.com>
2020-03-29 14:29:05 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './assets/style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import quizService from './assets/questions'
+import QuestionBox from './components/QuestionBox'
+import Result from "./components/result"
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            questionbank: [],
+            score: 0,
+            responses: 0
+
+        }
+
+    }
+    getQuestions = () => {
+        quizService().then(question => {
+            this.setState({
+                questionbank: question
+            })
+        })
+    }
+
+    playAgain = () => {
+        this.getQuestions()
+        this.setState({
+            score: 0,
+            responses: 0
+        })
+
+    }
+
+    computeAnswer = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            })
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        })
+    }
+
+    componentDidMount() {
+        this.getQuestions();
+    }
+
+    render() {
+
+        return (
+            <>
+                <div className="container">
+                    <div className="title"> React Quiz </div>
+                    {this.state.questionbank.length > 0 &&
+                        this.state.responses < 5 &&
+                        this.state.questionbank.map(({ question, answers, correct, questionID }) => (
+                            <h4>{<QuestionBox
+                                question={question}
+                                options={answers}
+                                keys={question.id}
+                                selected={answer => this.computeAnswer(answer, correct)}
+                            />}</h4>
+                        )
+                        )}
+                    {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain} />) : null}
+                </div>
+
+            </>
+        )
+    }
 }
 
-export default App;
+export default App
